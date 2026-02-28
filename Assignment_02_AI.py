76beafff46a6fbb6879f1d217b7b4576cba48e14
Saleha-Asghar <salehaasghar46@gmail.com>
2026-02-28 12:30:18 +0500

    Define Spot class with assignment-specific colors

---
+++ Assignment_02_AI.py
@@ -0,0 +1,41 @@
+import pygame
+
+
+WHITE = (255, 255, 255)   # Empty
+BLACK = (0, 0, 0)         # Obstacle (Wall)
+ORANGE = (255, 165, 0)    # Start Node
+TURQUOISE = (64, 224, 208)# Goal Node
+YELLOW = (255, 255, 0)    # Frontier (Priority Queue)
+RED = (255, 0, 0)         # Visited/Expanded
+GREEN = (0, 255, 0)       # Final Path
+GREY = (128, 128, 128)    # Grid Lines
+
+class Spot:
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self.x = row * width
+        self.y = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
+
+    def get_pos(self):
+        return self.row, self.col
+
+   # Identify state by color
+    def is_wall(self): return self.color == BLACK
+    def is_start(self): return self.color == ORANGE
+    def is_goal(self): return self.color == TURQUOISE
+
+    def make_start(self): self.color = ORANGE
+    def make_goal(self): self.color = TURQUOISE
+    def make_wall(self): self.color = BLACK
+    def make_visited(self): self.color = RED
+    def make_frontier(self): self.color = YELLOW
+    def make_path(self): self.color = GREEN
+    def reset(self): self.color = WHITE
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
